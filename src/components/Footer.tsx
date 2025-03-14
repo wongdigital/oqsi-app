9031ee16ce7b0fc61366c14283ddd9c4cfaf86d9
wongdigital <roger@wong.digital>
2025-03-14 16:40:19 -0700

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,7 +7,7 @@ export function Footer() {
       This is a fan-made project. The concepts, characters, and writing from &quot;Severance&quot; are the property of the show&apos;s producers and Apple. 
       Made with ♥ by <Link href="https://rogerwong.me" target="_blank" className="underline">Roger Wong</Link>
       &nbsp;•&nbsp;
-      <Link href="https://github.com/wongdigital/oqsi-frontend" target="_blank" className="underline">Github</Link>
+      <Link href="https://github.com/wongdigital/oqsi-app" target="_blank" className="underline">Github</Link>
     </span>
   );
 } 
